593a98b9c734dd8f9f99ca58c213906887e39be3
edyjen <edyjen@hotmail.com>
2016-03-05 02:21:22 -0430

    Demo template

--- html/mail/payment_accepted.php
+++ html/mail/payment_accepted.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
 <h1><?php echo $info["asunto_mailrevisado"]; ?></h1>
-<p><?php echo "Hola Sr(a)" . $data["RECIBE_NOMBRE"]; ?>,</p>
+<p><?php echo "Hola Sr(a) " . $data["RECIBE_NOMBRE"]; ?>,</p>
 <p>Su pago ha sido revisado y éste ha sido aprobado. Su producto está siendo preparado para su envío lo más pronto posible.</p>
 <p>Una vez realizado el envío, usted recibirá la información correspondiente.</p>
 <p>Saludos Cordiales</p>
